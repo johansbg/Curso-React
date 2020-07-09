2b3b690d1797f6637c404ecb83fe39a39eacb888
johansbg <jsburgos@uninorte.edu.co>
2020-07-09 10:30:19 -0500

    Semana 1 terminada

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -10,6 +10,11 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
+        console.log('Menu Component constructor is invoked');
+    }
+
+    componentDidMount(){
+        console.log('Menu Component componentDidMount is invoked');
     }
 
     onDishSelect(dish) {
@@ -36,8 +41,8 @@ class Menu extends Component {
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
+              <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card
                   onClick={() => this.onDishSelect(dish)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
@@ -48,6 +53,8 @@ class Menu extends Component {
             );
         });
 
+        console.log('Menu Component render is invoked');
+        
         return (
             <div className="container">
                 <div className="row">
