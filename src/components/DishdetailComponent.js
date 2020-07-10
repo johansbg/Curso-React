966a5290f30e82c3979cd8aae652b03efb69724f
johansbg <jsburgos@uninorte.edu.co>
2020-07-10 12:55:21 -0500

    tarea semana 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,24 +17,25 @@ import { Link } from 'react-router-dom';
     }
 
     function RenderComments({comments}){
-        const Comments = comments.map((comment) => 
-            <li key={comment.id} className="mb-3 mt-3">
-                <p>{comment.comment}</p>
-                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { 
-                    year: 'numeric', 
-                    month: 'short', 
-                    day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-            </li>
-        );
-        
         return (
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                <ul className="list-unstyled">{Comments}</ul>
+                <ul className="list-unstyled">
+                    {comments.map((comment) => {
+                        return(
+                            <li key={comment.id} className="mb-3 mt-3">
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { 
+                                year: 'numeric', 
+                                month: 'short', 
+                                day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        );
+                    })}</ul>
             </div>
         );
-        
     }
+
     const DishDetail = (props) => {
         if(props.dish!=null)
             return(
@@ -60,4 +61,5 @@ import { Link } from 'react-router-dom';
                 <div></div>
             );
     }
+    
 export default DishDetail;
